af55d6455db2f3f6920fe727441045ce8d488b97
rafitalavi <rafitalavi12345@gmail.com>
2024-07-12 12:22:12 +0600

    dataType

---
+++ variable &dataType/first.js
@@ -0,0 +1,30 @@
+console.log("alavi");
+name = "Rafit Alavi";
+console.log(name);
+isFollow=true
+console.log(isFollow)
+age = 24;
+console.log(age);
+let x = BigInt("123");
+console.log(x);
+let y = Symbol("alavi1")
+console.log(y);
+const student = {
+    fullName: "Rafit Alavi",
+    age: 20,
+    isPass : true,
+    cgpa : 3.37,
+
+};
+console.log(student.age);
+console.log(student["age"])
+student["age"]=student["age"]+1
+console.log(student.age)
+const products={
+    title :" Parker_Pen" ,
+    rating : 4,
+    offer: 5, 
+    price : 200,
+    isDeal:true,
+
+};
